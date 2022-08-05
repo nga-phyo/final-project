3f50e6d7d93f4ba5c5017048abaf4b74bcadae4f
Phyo Min Thant <phyominthant@Phyos-MacBook-Air.local>
2022-08-04 21:18:22 -0700

    Intital

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+
+@extends('layouts')
+
+@section('title', 'About Page')
+
+@section('text')
+<h1>@lang('hi')</h1>
+    
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+
+
+@extends('layouts')
+
+@section('title', 'About Page')
+
+@section('text')
+<h1>@lang('hello')</h1>
+    
+@endsection
